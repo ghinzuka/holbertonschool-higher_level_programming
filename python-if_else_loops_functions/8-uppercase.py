c65f4c0faeaff2f3eefa2f0417ba887175ec5e86
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-07 12:42:16 +0200

    correcting indent

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    uppercase_string = ""
     for char in str:
-        if 'a' <= char <= 'z':
-            uppercase_char = chr(ord(char) - 32)
-            uppercase_string += uppercase_char
-        else:
-            uppercase_string += char
-    print(uppercase_string)
+        if ord(char) >= ord('a') and ord(char) <= ord('z'):
+            char = chr(ord(char) - 32)
+        print("{:s}".format(char), end="")
+    print()
