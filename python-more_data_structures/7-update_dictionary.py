9948fe19f4271655b9a5b66814557deabf3ed738
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-14 10:59:58 +0200

    loop keys in dictionnary and check if i is equal to key then change the value else create the key and value

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -6,4 +6,4 @@ def update_dictionary(a_dictionary, key, value):
             return a_dictionary
         else:
             a_dictionary[key] = value
-            return a_dictionary
+        return a_dictionary
