56a689a5d4323b635e65cfa2c8e0f7dbfa66161f
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-14 20:07:54 +0200

    using three except to print messages and give the index of newlist 0 - pass the finally caus i dont use it return newlist

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = [0] * list_length
+    for i in range(list_length):
+        try:
+            new_list[i] = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            print("out of range")
+            new_list[i] = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list[i] = 0
+        except (ValueError, TypeError):
+            print("wrong type")
+            new_list[i] = 0
+        finally:
+            pass
+    return new_list
