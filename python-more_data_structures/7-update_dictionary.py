99f1654f4c1f7daaf89ea39ea3762adb6de3701d
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-14 11:07:10 +0200

    using another method to loop k and v in items

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for i in a_dictionary.items():
-        if i == key:
-            a_dictionary[i] = value
+    for k, v in a_dictionary.items():
+        if k == key:
+            a_dictionary[k] = value
             return a_dictionary
-        else:
-            a_dictionary[key] = value
-        return a_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
