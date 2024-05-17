0ee3eaf8a17e3f3ff3728b7a86a3dc9d416aebab
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-17 16:24:53 +0200

    trying another synthaxe to print the message in the right stderr thanks to fil=

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as error:
-        sys.stderr = error
-        print("Exception: {}".format(sys.stderr))
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
