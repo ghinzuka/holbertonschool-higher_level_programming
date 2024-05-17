bca85b83d8cbed93b7e49675cfbc40a974560338
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-17 11:25:23 +0200

    using another method 2

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,9 +1,9 @@
-def magic_calculation(a, b):
-    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+#!/usr/bin/ptyhon3
 
+def magic_calculation(a, b):
     if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
-    return sub(a, b)
+        return a + b
+    elif a > b:
+        return a - b
+    else:
+        return a * b
