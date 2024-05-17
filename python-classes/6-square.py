876057063adf96b683d5aa52546237ff17760ea3
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-17 18:01:25 +0200

    trying a method using 3 if to check if value of position is a tuple of two positif integers and print new line at position 0 - x and blank space at position x - 0

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -42,9 +42,19 @@ class Square:
         return self.__position
     @position.setter
     def position(self, value):
+        
         """set the value of the position in private
         args : value : the position
+        raise : type error if val is not int or value is not tuple
         """
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif not isinstance(value[0], int) and not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
         
 
     def area(self):
@@ -55,8 +65,14 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
-        """ print in stdout the square with the # char"""
+        """ print in stdout the square with the # char
+        or nothing in the position
+        """
+        for x in range(self.__position[1]):
+            print ()
         for i in range(self.__size):
+            for y in range(self.__position[0]):
+                print("", end=" ")
             for j in range(self.__size):
                 print('#', end="")
             print()
