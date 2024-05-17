b71c7990938d7228f772eacdd9c405b99a9a675b
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-17 08:34:46 +0200

    using only one print with format instead of 2

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(26):
+    result = ""
     if i % 2 == 0:
-        print(chr(ord('z') - i), end="")
+        result = chr(ord('z') - i)
     else:
-        print(chr(ord('Z') - i), end="")
+        result = chr(ord('Z') - i)
+    print("{}".format(result), end="")
