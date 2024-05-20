fa7bc3beaea59b5e85ef483346bf26cde9a4caf2
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-20 12:58:22 +0200

    changing main code using isintance instead of type

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,13 +11,12 @@ def add_integer(a, b=98):
     Add two integers or floats, converting them to integers if necessary.
     """
 
-    if type(a) not in [int, float]:
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-
     return (a + b)
