ddcd3f7e631de6d3a0ea60ed1adc34bf94af95f3
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-20 13:01:35 +0200

    changing another time my code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,12 +10,8 @@ def add_integer(a, b=98):
     """
     Add two integers or floats, converting them to integers if necessary.
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return (a + b)
+    return (int(a) + int(b))
