e5f791d88ed0ec8a6944eada5a3776e8097eef7f
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-20 11:58:08 +0200

    creating files coding the function and try to create a doctest file with several conditions

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ function that add two integer
-	b is set to 98 by default
-	if a or b are not float or int raise an error
-	return the sum of the int of a and b
+b is set to 98 by default
+if a or b are not float or int raise an error
+return the sum of the int of a and b
 """
 
 
@@ -10,13 +10,13 @@ def add_integer(a, b=98):
     """
     Add two integers or floats, converting them to integers if necessary.
     """
-    
+
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    
+
     a = int(a)
     b = int(b)
-    
+
     return (a + b)
