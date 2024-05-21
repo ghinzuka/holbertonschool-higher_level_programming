a30764909e147f42ea5c7a92134550af35ba4e76
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-21 20:23:10 +0200

    implementing all the errors - return the value to be print - adding plenty of examples in doctest

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""class with a method that raise and excpetion"""
+
+
+class BaseGeometry():
+    """class with a method to raise area"""
+    def area(self):
+        """ raises an Exception with the message """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """method that raise errors and get name the value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        else:
+            self.name = value
+            return (self.name)
