cf70340dcc20314c4f5ae33e20bfbf8b3960efd0
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-21 09:08:33 +0200

    trying to get the max integer thanks to the function assertEqual

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max(self):
+        self.assertEqual(max_integer([1, 2]), 2)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([1, 2, 5]), 5)
+        self.assertEqual(max_integer([-2, -4, -5, -6]), -2)
+        self.assertEqual(max_integer([2]), 2)
+
