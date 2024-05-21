52d9bb1dc467cfddb786edae9a563d25acb9930a
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-21 20:26:04 +0200

    removing the return cause its not asked in the project

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,6 +14,3 @@ class BaseGeometry():
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        else:
-            self.name = value
-            return (self.name)
