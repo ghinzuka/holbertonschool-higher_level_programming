ad1f5332697229e32e638e1f0ec518a761923078
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-21 10:47:34 +0200

    creating two private value of height and width

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -3,4 +3,39 @@
 
 
 class Rectangle:
-    """empty class named rectangle"""
+    """class rectangle with private widht and private lenght"""
+
+    def __init__(self, width=0, height=0):
+        """initiate widht and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieve the private value of with"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """get the value value of width if is int and positiv number"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """retrieve the private value of height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """get the value value of width if is int and positiv number"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
