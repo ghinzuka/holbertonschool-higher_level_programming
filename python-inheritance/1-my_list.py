fc3dd35cac9dee499572beede2f9e89204793370
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-21 17:24:00 +0200

    creating a class with no init in it and inherated from list atribute

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,5 +7,4 @@ class MyList(list):
     method print sorted : print in ascending order the list inherated"""
 
     def print_sorted(self):
-        res = sorted(self)
-        print(res)
+        print (sorted(self))
