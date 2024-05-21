bc669d0db1a9774e42a50a5edb51af2dad8f2c09
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-21 16:41:46 +0200

    changing readme

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""usign a method to return list"""
+
 
 def lookup(obj):
     """returns the list of available attributes and methods of an object"""
--- python-inheritance/README.md
+++ python-inheritance/README.md
@@ -1,1 +1,1 @@
-inheritance
+test
