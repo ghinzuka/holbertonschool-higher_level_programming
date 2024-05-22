c211e40b85c52a46a7fe67fb6a21ae82f691c7c3
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-22 16:28:33 +0200

    using another method

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,14 +5,12 @@ and raise error if value is not good"""
 
 class BaseGeometry:
     """class with a method to raise area"""
-
     def area(self):
         """ raises an Exception with the message """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """method that raise errors and return the value as name"""
-
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
