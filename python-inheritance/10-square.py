00bc8a77b3679f805a1de905fe87d190cf36f75b
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-22 09:33:18 +0200

    trying to implemen a class that inherit from a class that ihnerit from a class Oo

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""create a class named square that inherit from rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class square that inherit from rectangle in 9rectangle"""
+
+    def __init__(self, size):
+        """get size from rectangle"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """returns area of square"""
+        return (self.__size * self.__size)
