3c46e0c018a3a072ce616c0402e0916c2f346f17
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-22 08:57:31 +0200

    creating a new class inherated from basegeometry to get the integer validator and area method

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""class with a method that raise and excpetion
+and raise error if value is not good"""
+
+
+class BaseGeometry:
+    """class with a method to raise area"""
+
+    def area(self):
+        """ raises an Exception with the message """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """method that raise errors and return the value as name"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """class inherated from base geometry"""
+
+    def __init__(self, width, height):
+        """give height and width if integer validator is true"""
+        if self.integer_validator("width", width) is True:
+            self.__width = width
+        if self.integer_validator("height", height) is True:
+            self.__height = height
