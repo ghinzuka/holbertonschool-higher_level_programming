da86328af2bcafb812ba003f0b278656f583d1de
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-27 10:44:32 +0200

    opening as utf 8

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """open and print a file .txt"""
-    with open("my_file_0.txt", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
