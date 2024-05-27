19e3fc9125666a7cda2f8a10cd12500f8788d757
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-27 15:08:44 +0200

    trying with import json ineside def

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """function to return an object to a json file"""
-import json
 
 
 def save_to_json_file(my_obj, filename):
     """function to return an object to a json file"""
-    var = json.dumps(my_obj, ensure_ascii=False)
+    import json
     with open(filename, mode='w', encoding='utf-8') as file:
-        file.write(var)
+        file.write(json.dumps(my_obj, ensure_ascii=False))
