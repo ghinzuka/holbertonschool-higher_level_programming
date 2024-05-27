0f94845c9d61ffdfbbcb1fae436dc11f6c76296b
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-27 15:06:20 +0200

    removing a return

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,7 +5,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """function to return an object to a json file"""
-    var = json.dumps(my_obj)
+    var = json.dumps(my_obj, ensure_ascii=False)
     with open(filename, mode='w', encoding='utf-8') as file:
-        hello = file.write(var)
-    return hello
+        file.write(var)
