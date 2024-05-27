f7b13540d5e9bd8be1767eb8dadbc627b088306f
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-27 15:10:59 +0200

    deleting mode and utf8

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@
 def save_to_json_file(my_obj, filename):
     """function to return an object to a json file"""
     import json
-    with open(filename, mode='w', encoding='utf-8') as file:
+    with open(filename, 'w') as file:
         file.write(json.dumps(my_obj, ensure_ascii=False))
