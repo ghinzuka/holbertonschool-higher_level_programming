ec55696b2b489de63a6d09d87e222e13d35cd58b
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-27 15:24:45 +0200

    creating a def that return a json file out of object

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""defines function to return JSON representatin of an object"""
+"""function that returns JSON representation of an objec"""
 
 
 def to_json_string(my_obj):
-    """returns JSON representation of an object"""
+    """function that returns JSON representation of an object"""
     import json
     return json.dumps(my_obj)
