f7b4c8695d44fab6567b3981f5ac061171a8b4e6
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-28 16:59:07 +0200

    adding a goot intendation for the json file, adding the try and except and a sentence to return an error message

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -3,13 +3,18 @@
 
 import csv
 import json
+import autopep8
 
 
 def convert_csv_to_json(csv_filename):
-    with open(csv_filename, encoding='utf-8') as csv_file:
-        dictcsv = csv.DictReader(csv_file)
-        data = [row for row in dictcsv]
+    try:
+        with open(csv_filename, encoding='utf-8') as csv_file:
+            dictcsv = csv.DictReader(csv_file)
+            data = [row for row in dictcsv]
 
-    with open('data.json', mode='w', encoding='utf-8') as json_file:
-        json.dump(data, json_file, indent=4)
-    return True
+        with open('data.json', mode='w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, indent=4)
+        return True
+    except (FileNotFoundError, EOFError, AttributeError, OSError) as e:
+        print(f"An error occurred: {e}")
+        return False
