195f602afc6bdbb886b99e3ce98f2af32e18a485
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-28 12:47:07 +0200

    adding the line to append the first number to the following for each i in row

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -9,5 +9,8 @@ def pascal_triangle(n):
     triangle = [[1]]
     for i in range(0, n - 1):
         row = [1]
+        for j in range(0, i - 1):
+           row.append(triangle[i][j] + triangle[i][j + 1])
+        row.append(1)
         triangle.append(row)
     return triangle
