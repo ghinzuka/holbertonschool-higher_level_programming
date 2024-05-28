77e3db33be19c352b997593bf76eead98d321e91
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-28 10:35:59 +0200

    adding a def that replace the attributes of an object by attribtes of dict

---
+++ python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""class Student that defines a student"""
+
+
+class Student:
+    """class Student that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes the student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if type(attrs) is list:
+            new_dict = {}
+            for i in self.__dict__:
+                for j in attrs:
+                    if i == j:
+                        new_dict[i] = self.__dict__[i]
+            return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for key in json:
+            if key == "first_name":
+                self.first_name = json[key]
+            if key == "last_name":
+                self.last_name = json[key]
+            if key == "age":
+                self.age = json[key]
