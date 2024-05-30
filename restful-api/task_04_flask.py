1065c173bb6be94b0bc5a93094ee62400715602e
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-30 18:38:43 +0200

    adding a jsonify def for the route /data and a dynamic route to get the username from the data and the data corresping in a json way

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python3
 from flask import Flask
+from flask import jsonify
+from flask import request
+
+users = {
+    "Baptiste": {"name": "Baptiste", "age": 32, "city": "ayguemorte_les_graves"},
+    "Nicolas": {"name": "Nicolas", "age": 28, "city": "Bordeaux"},
+    "Matthieu": {"name": "Matthieu", "age": 26, "city": "Saint_denis_de_pile"},
+}
+
 
 app = Flask(__name__)
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
+
+@app.route("/data")
+def data():
+    usernames = list(users.keys())
+    return jsonify(usernames)
+
+@app.route("/status")
+def status():
+    return "OK"
+
+@app.route("/users/<username>")
+def user(username):
+	user = users.get(username)
+	if user:
+		return jsonify(user)
 
 if __name__ == "__main__": app.run()
