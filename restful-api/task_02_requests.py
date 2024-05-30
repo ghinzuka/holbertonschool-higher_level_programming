072f4de44bccf45d379d099b355fc367babf2604
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-30 12:33:32 +0200

    adding the function to save i na csv file, same as print but instead of printing only title, we create an empty list of dictionary that loop into each post, retrieve all atribute from each, append it to the empty list of dic then save it into a csv we create with columns names same as attributes retrieved and save the retrieve data from postdata

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import requests
 import json
+import csv
 
 
 def fetch_and_print_posts():
@@ -22,3 +23,35 @@ def fetch_and_print_posts():
         posts = response.json()
         for post in posts:
             print(post['title'])
+
+
+def fetch_and_save_posts():
+    """
+    Fetches posts from the JSONPlaceholder API and saves them to a CSV file.
+
+    This function sends a GET request to the JSONPlaceholder
+    API to retrieve a list of posts.
+    It then converts the response to JSON format and extracts
+    the necessary data (id, title, body) from each post.
+    The extracted data is then written to a CSV file
+    named 'posts.csv' with the headers 'id', 'title', and 'body'.
+
+    Returns:
+            None
+    """
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        posts = response.json()
+        posts_data = []
+        for post in posts:
+            post_dict = {
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            }
+            posts_data.append(post_dict)
+
+        with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(posts_data)
