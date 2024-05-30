2c588b6167b445497b40fae7cc52b15c6623e800
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-30 16:22:58 +0200

    adding two if, so if tthe path is the root, print the message, else if the path is data print the data with json dumps - changing send headers to get the right content type

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
 
 
 class Subclass(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header()
-        self.end_headers()
-        self.wfile.write(b"Hello, this is a simple API!")
-        
-        
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        if self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+
+
 httpd = HTTPServer(('', 8000), Subclass)
 httpd.serve_forever()
