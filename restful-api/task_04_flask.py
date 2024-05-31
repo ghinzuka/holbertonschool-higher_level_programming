54ec5bc9aa19d1a14470ee45997f3a2d90b6e192
ghinzuka <baptiste.pouquerou@gmail.com>
2024-05-31 07:57:59 +0200

    removing all data.... nosense

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,12 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 
 from flask import Flask, jsonify, request
-
-
 
 app = Flask(__name__)
 
 users = {}
-
 
 @app.route("/")
 def home():
@@ -14,22 +11,19 @@ def home():
     This function handles the root route of the Flask API.
 
     Returns:
-            str: A welcome message for the Flask API.
+        str: A welcome message for the Flask API.
     """
     return "Welcome to the Flask API!"
 
-
 @app.route("/data")
-def data():
+def get_data():
     """
-    Retrieve a list of usernames from the 'users'
-    dictionary and return it as a JSON response.
+    Retrieves and returns the data from the 'users' dictionary as a JSON response.
 
     Returns:
-            A JSON response containing a list of usernames.
+        A JSON response containing the list of keys from the 'users' dictionary.
     """
-    usernames = list(users.keys())
-    return jsonify(usernames)
+    return jsonify(list(users.keys()))
 
 
 @app.route("/status")
@@ -38,24 +32,22 @@ def status():
     Returns the status of the API.
 
     Returns:
-            str: The status message "OK".
+        str: The status message "OK".
     """
     return "OK"
 
 
 @app.route("/users/<username>")
-def user(username):
+def get_user(username):
     """
     Retrieve user information based on the provided username.
 
     Args:
-            username: The username of the user to retrieve information for.
+        username (str): The username of the user to retrieve.
 
     Returns:
-            If the user is found, the user information
-            is returned as a JSON response.
-            If the user is not found, a JSON response with an
-            error message and a 404 status code is returned.
+        If the user is found, returns a JSON response containing the user information.
+        If the user is not found, returns a JSON response with an error message and a 404 status code.
     """
     user = users.get(username)
     if user:
@@ -63,33 +55,21 @@ def user(username):
     else:
         return jsonify({"error": "User not found"}), 404
 
-
 @app.route("/add_user", methods=["POST"])
 def add_user():
     """
     Add a new user to the system.
 
     Returns:
-            A JSON response with the following structure:
-            - If the JSON data is invalid: {"error": "Invalid JSON data"}
-            - If the username already exists:
-            {"error": "Username already exists"}
-            - If the user is successfully added:
-            {"message": "User added", "user": <user_data>}
-            - If an exception occurs: {"error": <exception_message>}
+        A JSON response containing a success message and the user data if the user is added successfully.
+        A JSON response containing an error message if the username is missing.
     """
-    new_user = request.get_json()
-    if not new_user:
-        return jsonify({"error": "Invalid JSON data"}), 400
-    username = new_user.get("username")
-    if username in users:
-        return jsonify({"error": "Username already exists"}), 409
-    try:
-        users[username] = new_user
-        return jsonify({"message": "User added", "user": users[username]}), 201
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
+    user_data = request.get_json()
+    username = user_data.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    users[username] = user_data
+    return jsonify({"message": "User added", "user": user_data}), 201
 
 if __name__ == "__main__":
     app.run()
