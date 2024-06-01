52584fc58a0e1df7e4d8ed9148e77a76182225d8
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-01 19:03:04 +0200

    adding the client part connection to the same host and port - serialize the data to send it to the server - then the server will receive teh data with the coon.recv we coded before and json load it out and print it

--- python-serialization/task_04_net.py
+++ python-serialization/task_04_net.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-""""""
 import socket
 import json
 
@@ -31,3 +30,20 @@ def start_server(host='localhost', port=65432):
         finally:
             conn.close()
             print(f"Connection with {addr} closed")
+
+def send_data(data_dict, host='localhost', port=65432):
+  
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((host, port))
+        
+        serialized_data = json.dumps(data_dict).encode('utf-8')
+        client_socket.sendall(serialized_data)
+        
+    except ConnectionError as e:
+        print(f"Connection error: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        client_socket.close()
+        print("Connection closed")
