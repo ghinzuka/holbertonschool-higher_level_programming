15ea90284fa760e695c0bedffb7210c02080b7e8
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-17 15:46:36 +0200

    creating database and a table in it

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,6 @@
--- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
--- 2. Delete a database
+-- Write a script that creates a table called first_table in the current database in your MySQL server.
+-- 4. First table
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT ,
+	name VARCHAR(256)
+);
