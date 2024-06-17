0e837d804527453a2089a0e12422f8f60ab1005f
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-17 15:34:27 +0200

    ok i just removed the use of mysql cause we pass it as a argument

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,8 +1,5 @@
 -- WWrite a script that lists all the tables of a database in your MySQL server.
 -- 3. List tables
 
--- Connect to MySQL and select the database
-USE mysql;
-
 -- Show all tables in the selected database
 SHOW TABLES;
