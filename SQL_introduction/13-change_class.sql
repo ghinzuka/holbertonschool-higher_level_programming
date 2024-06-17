9a0a3eb5a4b8bd68f885fec35ef82f66a7028a52
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-17 17:13:15 +0200

    deleting the rows where the score is under 5 ....too bad for them

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,2 +1,3 @@
--- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
--- 2. Delete a database
+-- Write a script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- 13. Score too low
+DELETE FROM second_table WHERE score <=5; 
