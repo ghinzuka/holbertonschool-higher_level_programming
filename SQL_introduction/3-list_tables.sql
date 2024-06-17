a406fa8a8ff69198d351e02569499c89fda1b823
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-17 15:32:15 +0200

    using another way to code it in two lines, cause the chercker do not like it

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,8 @@
 -- WWrite a script that lists all the tables of a database in your MySQL server.
 -- 3. List tables
-SHOW TABLES FROM mysql;
+
+-- Connect to MySQL and select the database
+USE mysql;
+
+-- Show all tables in the selected database
+SHOW TABLES;
