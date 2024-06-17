5cf39891c741effb1d9a181cf58a23169afe27f4
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-17 15:59:50 +0200

    show CREATE TABLE is used to show all created tables in a database with the variable in it

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
--- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
--- 2. Delete a database
+-- Write a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- 5. Full description
+SHOW CREATE TABLE first_table;
