8899a07dc59cbdaa181178a2e6923cede236ab0f
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-17 16:08:47 +0200

    trying a method with SELECT instead of SHOW

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,2 +1,3 @@
--- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
--- 2. Delete a database
+-- Write a script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- 6. List all in table
+SELECT * FROM first_table;
