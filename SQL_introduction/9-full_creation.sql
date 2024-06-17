64f4b4fedf9f734476d3129570906addca070b76
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-17 16:36:43 +0200

    creating the seconde table and adding 1 b 1 the news rows

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,2 +1,12 @@
--- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
--- 2. Delete a database
+-- Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+-- 9. Full creation
+CREATE TABLE second_table if NOT EXISTS (
+	id INT,
+	name VARCHAR(256),
+	score(INT)
+);
+-- add 4 rows
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
