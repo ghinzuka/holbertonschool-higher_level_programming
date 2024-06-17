e8a61450b2f5a65d7439cb4fd8da8594917c1881
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-17 17:07:55 +0200

    updating the second table SETting the score to 10 where the name is bob

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,3 @@
--- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
--- 2. Delete a database
+-- Write a script that updates the score of Bob to 10 in the table second_table.
+-- 12. Cheating is bad
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
