e95408f93a46b72af04a73f78b9645f06fd07cab
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-17 16:44:05 +0200

    same as the 6 with the select tool - from - and finally the ORDER BY score and DESCending order

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,3 @@
--- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
--- 2. Delete a database
+-- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- 10. List by best
+SELECT score, name FROM second_table ORDER BY score Desc;
