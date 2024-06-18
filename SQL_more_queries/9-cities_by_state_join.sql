4d36433b6e75a738703ee2cc03312444f4f1441c
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-18 15:53:37 +0200

    using INNER JOIN instead of WHERE citie.statid = states.id

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -5,5 +5,7 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT cities.id, cities.name, states.name 
-FROM cities, states 
+FROM cities
+	INNER JOIN states
+	ON cities.state_id = states.id
 ORDER BY cities.id ASC;
