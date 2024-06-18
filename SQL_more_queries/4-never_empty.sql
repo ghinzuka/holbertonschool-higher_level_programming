b03224872ee15434a2f1c706c97196ec8106d3a2
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-18 13:27:53 +0200

    adding a new contraint DEFAULT so it will never be NULL

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,3 @@
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
+-- Write a script that creates the table id_not_null on your MySQL server.
+-- 4. ID can't be null
+CREATE TABLE IF NOT EXISTS id_not_null (id INT NOT NULL DEFAULT 1, name VARCHAR (256));
