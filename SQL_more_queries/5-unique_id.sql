d08299689de640a9384227042a579dc752c0fde6
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-18 13:33:04 +0200

    adding the UNIQUE Constraint before the DEFAULT option

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,3 @@
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
+-- Write a script that creates the table unique_id on your MySQL server.
+-- 5. Unique ID
+CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE DEFAULT 1, name VARCHAR(256));
