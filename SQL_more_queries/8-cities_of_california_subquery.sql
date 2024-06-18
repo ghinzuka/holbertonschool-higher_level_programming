98403bcce2cee1b952b63c79f1a0d39d2e0a8f2d
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-18 15:16:07 +0200

    creating more entry so the exercice would be more comprehensiv, this is a subquery using in the where statement another query from the states table

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,14 @@
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
+-- 8. Cities of California
+-- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+-- The states table contains only one record where name = California (but the id can be different, as per the example)
+-- Results must be sorted in ascending order by cities.id
+-- You are not allowed to use the JOIN keyword
+-- The database name will be passed as an argument of the mysql command
+SELECT id, name 
+FROM cities 
+WHERE state_id = (
+    SELECT id 
+    FROM states 
+    WHERE name = 'California'
+)
+ORDER BY id ASC;
