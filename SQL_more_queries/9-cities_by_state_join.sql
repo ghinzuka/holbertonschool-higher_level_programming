5669a86cc35b564df83f607170223aa86700dd8a
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-18 15:36:22 +0200

    trying to use only one select with cities.id and cities.name but i have 3 times the same data

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,9 @@
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
+-- 9. Cities by States
+-- Write a script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT cities.id, cities.name, states.name 
+FROM cities, states 
+ORDER BY cities.id ASC;
