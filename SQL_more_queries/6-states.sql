05392d4674623c3f9d2c44526537dcbdcf6365ff
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-18 13:44:45 +0200

    using several constrain and option like UNIQUE auto increment (so it will always ne unique BTW) primary key and not NULL (it cannot be by the autoincrement)

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,2 +1,8 @@
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
+-- 6. States table
+-- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	name VARCHAR(256) NOT NULL
+);
