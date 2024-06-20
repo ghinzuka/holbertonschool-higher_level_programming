41fab372ded03e182faa2ad6129a113b55c8f885
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-20 12:33:40 +0200

    pycode

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,10 @@
 Connects to a MySQL database and selects all states from the 'states' table.
 
 Args:
-	username (str): The username for the MySQL database.
-	password (str): The password for the MySQL database.
-	database (str): The name of the MySQL database.
-	"""
+username (str): The username for the MySQL database.
+password (str): The password for the MySQL database.
+database (str): The name of the MySQL database.
+"""
 
 if __name__ == "__main__":
     import sys
