cdecce25117d8019fd0f8f2edb73640baa699b08
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-21 20:05:55 +0200

    adding comment

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,9 @@
 -- 18. Temperatures #0
 -- Write a script that displays the average temperature 
 -- (Fahrenheit) by city ordered by temperature (descending).
+-- using the the AVG abregation tu get the average temp - 
+-- GROUP by the city so it knows from wich group of (values) it needs to do the AVG from - 
+-- order by the avg temp in descending order
 SELECT
     city,
     AVG(value) AS avg_temp
