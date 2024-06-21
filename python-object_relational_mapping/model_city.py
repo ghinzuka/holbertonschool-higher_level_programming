96e601caf615d5e9701ddb20e0ce7d682c1a0aa1
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-21 18:50:16 +0200

    correcting pycode style

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,10 +3,10 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City(Base):
     """State class"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    
