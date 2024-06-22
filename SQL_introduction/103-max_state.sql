ad77c14c6017d9bbf38649c560f17043706ecf02
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-22 09:12:42 +0200

    changign readme

--- SQL_introduction/103-max_state.sql
+++ SQL_introduction/103-max_state.sql
@@ -1,5 +1,7 @@
 -- 20. temperature 2
 -- Write a script that displays the max temperature of each state (ordered by State name).
+-- select the state - we use the abregation MAX instead of AVG from the temperature - 
+-- group by state instead of cities and order by state ascending'
 SELECT
     state,
     MAX(value) AS max_temp
