49371d609bb65f886bf0317aa11e7d8ab6abe671
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-22 12:16:37 +0200

    trying another way to approach it : two let join instead of select and where- we de first join to take the id of the genre where the id is NULL (so everything except comedy) - then we do the second join to link the id of the show_genre to the show id

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -6,9 +6,10 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT DISTINCT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres 
-ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres 
-ON tv_show_genres.genre_id = tv_genres.id AND tv_genres.name = 'Comedy'
-WHERE tv_genres.id IS NULL
+WHERE tv_shows.id NOT IN (
+    SELECT tv_show_genres.show_id
+    FROM tv_show_genres
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
 ORDER BY tv_shows.title ASC;
