2c1e367fbcb46f9ff049178ed3998fe7ea1bd411
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-24 08:08:29 +0200

    adding pycode style

---
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Lists all State objects and corresponding City objects
+from the database hbtn_0e_101_usa.
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == "__main__":
+    
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(username, password, database)
+    )
+    
+    Base.metadata.bind = engine
+    
+    session = Session(bind=engine)
+    
+    try:
+        states = session.query(State).order_by(State.id).all()
+        
+        for state in states:
+            print(f"{state.id}: {state.name}")
+            for city in state.cities:
+                print(f"    {city.id}: {city.name}")
+    
+    finally:
+        session.close()
