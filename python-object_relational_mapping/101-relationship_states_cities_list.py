ba101e3056dd925f0621e4908023a485767b82b3
ghinzuka <baptiste.pouquerou@gmail.com>
2024-06-24 08:09:01 +0200

    adding pycode style

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -10,7 +10,7 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -19,18 +19,18 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(username, password, database)
     )
-    
+
     Base.metadata.bind = engine
-    
+
     session = Session(bind=engine)
-    
+
     try:
         states = session.query(State).order_by(State.id).all()
-        
+
         for state in states:
             print(f"{state.id}: {state.name}")
             for city in state.cities:
                 print(f"    {city.id}: {city.name}")
-    
+
     finally:
         session.close()
