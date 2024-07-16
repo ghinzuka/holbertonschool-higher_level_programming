2024aafb684ed4801d22a9275afb5e4fb7d2d1ef
ghinzuka <baptiste.pouquerou@gmail.com>
2024-07-16 10:02:48 +0200

    adding product.json file

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
-import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
@@ -15,12 +14,5 @@ def about():
 def contact():
     return render_template('contact.html')
 
-@app.route('/items')
-def items():
-    with open('items.json') as f:
-        data = json.load(f)
-        items_list = data.get("items", [])
-        return render_template('items.html', items=items_list)
-
 if __name__ == '__main__':
-	app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)
